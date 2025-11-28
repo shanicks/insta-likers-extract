5855caec518401dcd157119c21134c053d846fa7
shantanurathod <rathod.shantanu2k@gmail.com>
2025-11-28 18:19:37 +0530

    debugging false

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def undo():
 # -------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
